cfe1e9cdc475e638b95ab3668601e2620b814407
Ruy Brito <ruybrito106@users.noreply.github.com>
2016-06-12 19:07:40 -0300

    Update RakingRequest.java

--- app/src/main/java/br/ufpe/cin/pet/geoquest/RakingRequest.java
+++ app/src/main/java/br/ufpe/cin/pet/geoquest/RakingRequest.java
@@ -5,12 +5,10 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.util.Log;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.protocol.HTTP;
 import org.apache.http.util.EntityUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
